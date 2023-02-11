e803a26615f05834b071348d6a08065b36b9e543
M.J <89101458+Maytham-MJ@users.noreply.github.com>
2023-02-11 13:51:01 -0800

    Delete src directory

--- src/main/java/com/MyMusic/API/repository/HappySongs.java
+++
@@ -1,8 +0,0 @@
-package com.MyMusic.API.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HappySongs extends JpaRepository<HappySongs, Integer> {
-}
